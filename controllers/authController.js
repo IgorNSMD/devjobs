84092fa3407002c7a9be11f633d3099cf2438dee
IgorNSMD <insanmartind@gmail.com>
2023-03-02 20:33:26 -0300

    Capitulo 257

--- controllers/authController.js
+++ controllers/authController.js
@@ -93,3 +93,51 @@ exports.enviarToken = async (req, res) => {
     req.flash('correcto', 'Revisa tu email para las indicaciones');
     res.redirect('/iniciar-sesion');
 }
+
+// Valida si el token es valido y el usuario existe, muestra la vista
+exports.reestablecerPassword = async (req, res) => {
+    const usuario = await Usuarios.findOne({
+        token : req.params.token,
+        expira: {
+            $gt: Date.now()
+        }
+    });
+
+    if(!usuario) {
+        req.flash('error', 'El formulario ya no es valido, intenta de nuevo');
+        return res.redirect('/reestablecer-password');
+    }
+
+    // Todo bien, mostrar el formulario
+    res.render('nuevo-password', {
+        nombrePagina : 'Nuevo Password'
+    })
+}
+
+// almacena el nuevo password en la BD
+exports.guardarPassword = async (req, res) => {
+    const usuario = await Usuarios.findOne({
+        token : req.params.token,
+        expira: {
+            $gt: Date.now()
+        }
+    });
+
+    // no existe el usuario o el token es invalido
+    if(!usuario) {
+        req.flash('error', 'El formulario ya no es valido, intenta de nuevo');
+        return res.redirect('/reestablecer-password');
+    }
+
+    // Asignar nuevo password, limpiar valores previos
+    usuario.password = req.body.password;
+    usuario.token = undefined;
+    usuario.expira = undefined;
+
+    // agregar y eliminar valores del objeto
+    await usuario.save();
+
+    // redirigir
+    req.flash('correcto', 'Password Modificado Correctamente');
+    res.redirect('/iniciar-sesion');
+}
